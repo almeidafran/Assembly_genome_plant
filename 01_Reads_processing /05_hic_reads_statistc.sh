5cb00bc8ec3aba3fbea13c70c8d20c8d8cbcc246
Francine Almeida <139783093+almeidafran@users.noreply.github.com>
2026-07-06 11:01:19 -0300

    Update 05_hic_reads_statistc.sh

--- 01_Reads_processing /05_hic_reads_statistc.sh
+++ 01_Reads_processing /05_hic_reads_statistc.sh
@@ -6,3 +6,23 @@ conda activate yahs
 
 ##### simple statistics of FASTA/Q files #####
 seqkit stats -a -j 2 $hic/hic.R1.fq.gz $hic/hic.R2.fq.gz > hic_stats.txt
+
+####### Hi-C read QC #####
+fastqc \
+  -t 16 \
+  -o 00_raw_qc \
+  $hic/HiC_R1.fastq.gz $hic/HiC_R2.fastq.gz
+
+multiqc 00_raw_qc -o 00_raw_qc/multiqc_hic
+
+### Trimming of Hi-C reads (if necessary) #####
+fastp \
+  -i $hic/HiC_R1.fastq.gz \
+  -I $hic/HiC_R2.fastq.gz \
+  -o HiC_R1.trim.fastq.gz \
+  -O HiC_R2.trim.fastq.gz \
+  --detect_adapter_for_pe \
+  --thread 16 \
+  --html 00_raw_qc/fastp_hic.html \
+  --json 00_raw_qc/fastp_hic.json
+
